a563617cc25ad81fb53262e702c1596b4c1325ad
quantity <hailhailHaily@hotmail.com>
2017-05-12 09:26:15 +0800

    adding photo capture practice

---
+++ ev4/ev4_camera/app/src/main/java/com/example/ytt/ev4_camera/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.ytt.ev4_camera;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setup_storage();
+    }
+
+
+    //---------------------- setting up
+
+    private void setup_storage(){
+
+
+    }
+
+
+
+
+
+
+    //---------------------- GUI referencing methods
+
+
+    public void take_picture(View v){
+
+
+    }
+
+
+    public void view_gallery(View v){
+
+    }
+
+
+}
---
+++ ev4/ev4_camera/app/src/main/java/com/example/ytt/ev4_camera/Prefs/Ev4Prefs.java
@@ -0,0 +1,12 @@
+package com.example.ytt.ev4_camera.Prefs;
+
+import android.os.Environment;
+
+/**
+ * Created by ytt on 2017/5/12.
+ */
+
+public class Ev4Prefs {
+
+    public static final String IMG_DIR= Environment.getExternalStorageDirectory()+
+}
